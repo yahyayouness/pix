d53e1a8f875ed37b54391c7bf7cbcec1da1bb768
Krysthalia <ang.schall@gmail.com>
2019-11-12 09:47:37 +0100

    [pf-118] wip button and tests

--- admin/tests/unit/controllers/authenticated/certifications/sessions/info/index-test.js
+++ admin/tests/unit/controllers/authenticated/certifications/sessions/info/index-test.js
@@ -1,12 +1,49 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+// import Service from '@ember/service';
+import sinon from 'sinon';
 
 module('Unit | Controller | authenticated/certifications/sessions/info/index', function(hooks) {
+
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    const controller = this.owner.lookup('controller:authenticated/certifications/sessions/info/index');
-    assert.ok(controller);
+  let controller;
+
+  hooks.beforeEach(function() {
+    controller = this.owner.lookup('controller:authenticated/certifications/sessions/info/index');
   });
+
+  module('#downloadSessionResultFile', function() {
+
+    test('should launch the download of result file', function(assert) {
+      assert.expect(1);
+
+      // given
+      controller.set('sessionInfoService');
+      const sessionInfoServiceMock = sinon.mock(controller.get('sessionInfoService'), 'downloadSessionExportFile');
+      const model = {}; // this.model ... ?
+
+      // when
+      controller.actions.downloadSessionResultFile.call(controller);
+      // ou ?
+      // controller.send('downloadSessionResultFile');
+
+      // then
+      sinon.assert.calledWithExactly(sessionInfoServiceMock, model);
+    });
+
+    // test('should show an error', function() {
+    //   // given
+    //   const notificationsMock = sinon.mock(controller.get('notifications'), 'error');
+    //   controller.set(notificationsMock);
+    //   const error = 'an error';
+    //
+    //   // when
+    //   controller.actions.downloadSessionResultFile.call(controller);
+    //
+    //   // then
+    //   sinon.assert.calledWithExactly(notificationsMock, error);
+    // });
+  });
+
 });
